19347474838792524c854badf8a85a305853986b
EC2 Default User <ec2-user@ip-172-31-8-45.us-east-2.compute.internal>
2025-03-29 19:55:02 +0000

    restarting

--- src/components/common/index.jsx
+++
@@ -1,4 +0,0 @@
-// filepath: /my-react-app/my-react-app/src/components/common/index.jsx
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Card } from './Card';
--- src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-// filepath: /my-react-app/my-react-app/src/hooks/index.js
-export const useCustomHook = () => {
-    // Custom hook logic here
-};
