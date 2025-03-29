9d11702e96ca6c90321e890876f1de3b518c350a
jfranklin-153 <jfranklin@universalconnectivity.com>
2025-03-29 15:20:10 -0400

    removed .css reference

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-// filepath: /my-react-app/my-react-app/src/components/App.jsx
 import React from 'react';
-import './App.css'; // Assuming you have some styles for this component
 import MainPage from '../pages/index';
 
 const App = () => {
